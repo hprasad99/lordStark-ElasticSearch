f7882bb56558cbe9495a2d44144a267d58ad79e8
hprasad99 <himanshu.prasad@ril.com>
2022-09-20 12:09:25 +0530

    fetch suggestion

--- src/main/java/services/ProductSearchService.java
+++ src/main/java/services/ProductSearchService.java
@@ -2,14 +2,20 @@ package services;
 
 import lombok.extern.slf4j.Slf4j;
 import models.Product;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.IndexedObjectInformation;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.stereotype.Service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +26,63 @@ public class ProductSearchService {
 
     private final ElasticsearchOperations elasticsearchOperations;
 
+    public void findProductsByBrand(final String brandName) {
+        QueryBuilder queryBuilder = QueryBuilders.matchQuery("manufacturer",brandName);
+
+        Query searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(queryBuilder)
+                .build();
+
+        SearchHits<Product> productHits = elasticsearchOperations
+                .search(searchQuery,
+                        Product.class,
+                        IndexCoordinates.of(PRODUCT_INDEX));
+
+        log.info("productHits {} {}", productHits.getSearchHits().size(), productHits.getSearchHits());
+
+        List<SearchHit<Product>> searchHits =
+                productHits.getSearchHits();
+        int i=0;
+        for(SearchHit<Product> searchHit: searchHits) {
+            log.info("searchHit {}", searchHit);
+        }
+    }
+
+    public void findByProductName(final String productName) {
+        Query searchQuery = new StringQuery(
+                "{\"match\":{\"name\":{\"query\":\""+ productName + "\"}}}\"");
+        SearchHits<Product> products = elasticsearchOperations.search(searchQuery, Product.class, IndexCoordinates.of(PRODUCT_INDEX));
+    }
+
+    public void findByProductPrice(final String productPrice) {
+        Criteria criteria = new Criteria("price").greaterThan(10.0).lessThan(100.0);
+        Query searchQuery = new CriteriaQuery(criteria);
+
+        SearchHits<Product> products = elasticsearchOperations.search(searchQuery, Product.class, IndexCoordinates.of(PRODUCT_INDEX));
+    }
+
+    public List<Product> processSearch(final String query) {
+        log.info("Search with query {}", query);
+
+        QueryBuilder queryBuilder =
+                QueryBuilders
+                        .multiMatchQuery(query, "name", "description")
+                        .fuzziness(Fuzziness.AUTO);
+
+        Query searchQuery = new NativeSearchQueryBuilder()
+                .withFilter(queryBuilder)
+                .build();
+
+        SearchHits<Product> productSearchHits = elasticsearchOperations
+                .search(searchQuery, Product.class, IndexCoordinates.of(PRODUCT_INDEX));
+
+        List<Product> productMatches = new ArrayList<Product>();
+        productSearchHits.forEach(searchHit -> {
+            productMatches.add(searchHit.getContent());
+        });
+
+        return productMatches;
+    }
     public ProductSearchService(ElasticsearchOperations elasticsearchOperations) {
         super();
         this.elasticsearchOperations = elasticsearchOperations;
@@ -40,4 +103,26 @@ public class ProductSearchService {
         return documentId;
     }
 
+    public List<String> fetchSuggestions(String query) {
+        QueryBuilder queryBuilder = QueryBuilders
+                .wildcardQuery("name", query + "*");
+        Query searchQuery = new NativeSearchQueryBuilder()
+                .withFilter(queryBuilder)
+                .withPageable(PageRequest.of(0,5))
+                .build();
+
+        SearchHits<Product> searchSuggestions =
+                elasticsearchOperations
+                        .search(searchQuery,
+                                Product.class,
+                                IndexCoordinates.of(PRODUCT_INDEX)
+                        );
+
+        List<String> suggestions = new ArrayList<String>();
+
+        searchSuggestions.getSearchHits().forEach(searchHit->{
+            suggestions.add(searchHit.getContent().getName());
+        });
+        return suggestions;
+    }
 }
