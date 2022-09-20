5d1f80bd0e38f56267c105429cd0edb7ce24deef
hprasad99 <himanshu.prasad@ril.com>
2022-09-20 12:24:25 +0530

    implemented UI Controller

--- src/main/java/controllers/UIController.java
+++ src/main/java/controllers/UIController.java
@@ -1,4 +1,35 @@
 package controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import services.SearchService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+@Controller
+@Slf4j
 public class UIController {
+
+    private final SearchService searchService;
+    @Autowired
+    public UIController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/search")
+    public String home(Model model){
+        List<Product> products = searchService.fetchProductNamesContaining("Hornby");
+
+        List<String> names = products.stream().flatMap(product -> {
+            return Stream.of(product.getName());
+        }).collect(Collectors.toList());
+        log.info("product names {}", names);
+        model.addAttribute("names", names);
+        return "search";
+    }
 }
